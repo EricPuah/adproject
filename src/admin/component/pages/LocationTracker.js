3e9c6965c0c92f284fc707b6169369d6856819f8
Eric Puah <ericpuah926@gmail.com>
2023-11-28 23:30:24 +0800

    new

--- src/admin/component/pages/LocationTracker.js
+++ src/admin/component/pages/LocationTracker.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import 'leaflet.css';
 import io from 'socket.io-client';
 import './LocationTracker.css';
 
