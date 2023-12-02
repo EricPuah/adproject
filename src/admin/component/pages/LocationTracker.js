83ccc94be692a9a3a29a9e78d8d70ed64a2eff10
Eric Puah <ericpuah926@gmail.com>
2023-12-02 19:04:29 +0800

    new

--- src/admin/component/pages/LocationTracker.js
+++ src/admin/component/pages/LocationTracker.js
@@ -22,9 +22,26 @@ const center = {
 const staticMarkers = [
   { position: { lat: 1.5593613531032313, lng: 103.63280919934147 }, name: 'KRP 1' },
   { position: { lat: 1.5593613531032313, lng: 103.63280919934147 }, name: 'KRP 1' },
-  { position: { lat: 1.5594488031178655, lng: 103.63181397038748}, name: 'KRP 2' },
-  { position: { lat: 1.5581984657886114, lng: 103.63013361402903}, name: 'KRP 3' },
-  { position: { lat: 1.557820767476252,  lng: 103.62933025021609}, name: 'KRP 4' },
+  { position: { lat: 1.5594488031178655, lng: 103.63181397038748 }, name: 'KRP 2' },
+  { position: { lat: 1.5581984657886114, lng: 103.63013361402903 }, name: 'KRP 3' },
+  { position: { lat: 1.557820767476252, lng: 103.62933025021609 }, name: 'KRP 4' },
+  { position: { lat: 1.5583386592877633, lng: 103.62772119148106 }, name: 'PKU' },
+  { position: { lat: 1.5666014101927015, lng: 103.62692956217548 }, name: 'KDSE 1' },
+  { position: { lat: 1.5664661331768341, lng: 103.62710887123512 }, name: 'KDSE 2' },
+  { position: { lat: 1.5680164884613657, lng: 103.62444943229734 }, name: 'KDSE 3' },
+  { position: { lat: 1.5600993131033718, lng: 103.62924840747273 }, name: 'KTF 1' },
+  { position: { lat: 1.5611031811814065, lng: 103.63092734040339 }, name: 'KTF 2' },
+  { position: { lat: 1.559717628544402, lng: 103.63474936112164 }, name: 'Central Point' },
+  { position: { lat: 1.5612972023347578, lng: 103.63259352721195 }, name: 'Arked Cengal 1' },
+  { position: { lat: 1.5611806042328233, lng: 103.63249300698176 }, name: 'Arked Cengal 2' },
+  { position: { lat: 1.5616945552654073, lng: 103.62929878111245 }, name: 'KTR 1' },
+  { position: { lat: 1.5627521550241115, lng: 103.62758295044863 }, name: 'KTR 2' },
+  { position: { lat: 1.5637730222723685, lng: 103.62742170059452 }, name: 'KTR 3' },
+  { position: { lat: 1.5648820767159544, lng: 103.62786828500501 }, name: 'KTR 4' },
+  { position: { lat: 1.5627194781848628, lng: 103.63643945417387 }, name: 'FKA' },
+  { position: { lat: 1.5626948582674094, lng: 103.63913654898062}, name: 'N24' },
+  { position: { lat: 1.5603335706721926, lng: 103.64158337666292 }, name: 'P19' },
+
 ];
 
 function LocationTracker() {
