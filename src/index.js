8df32a28dbf6c98bef796f4daded0294e4623f5e
Eric Puah <ericpuah926@gmail.com>
2024-01-10 15:31:59 +0800

    bew

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from 'react-auth-kit'; //npm install react-auth-kit@^2.0.0
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
 root.render(
   <React.StrictMode>
     <AuthProvider
